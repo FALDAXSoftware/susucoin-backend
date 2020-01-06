ad5776ccce7a8702687e3abf223a0f2bf1ac51d9
Kishan_Vadodaria <kishan.vadodaria@openxcellinc.com>
2020-01-06 14:12:05 +0530

    pull commit

--- controllers/v1/CoinsController.js
+++ controllers/v1/CoinsController.js
@@ -16,6 +16,7 @@ var Helper = require("../../helpers/helpers");
 var addressHelper = require("../../helpers/get-new-address");
 var sendHelper = require("../../helpers/send");
 var balanceHelper = require("../../helpers/get-wallet-balance");
+var getReceiveByAddressHelper = require("../../helpers/get-receiveby-address");
 const constants = require('../../config/constants');
 // Controllers
 var { AppController } = require('./AppController');
@@ -159,65 +160,67 @@ class UsersController extends AppController {
                 .andWhere('type', 2)
                 .orderBy('id', 'DESC')
 
-            if (coinData != undefined) {
-                var walletData = await WalletModel
-                    .query()
-                    .first()
-                    .where("deleted_at", null)
-                    .andWhere("user_id", user_id)
-                    .andWhere("coin_id", coinData.id)
-                    .orderBy('id', 'DESC')
+            if (coinData != undefined) {    
 
-                console.log("walletData", walletData)
+                    var walletData = await WalletModel
+                        .query()
+                        .first()
+                        .where("deleted_at", null)
+                        .andWhere("user_id", user_id)
+                        .andWhere("coin_id", coinData.id)
+                        .orderBy('id', 'DESC')
 
-                if (walletData != undefined) {
+                    console.log("walletData", walletData)
 
-                    var sendObject = {
-                        "address": walletData.send_address,
-                        "amount": amount,
-                        "message": "test"
+                    if (walletData != undefined) {
+
+                        var sendObject = {
+                            "address": walletData.send_address,
+                            "amount": amount,
+                            "message": "test"
+                        }
+
+                        var userReceiveAddress = await sendHelper.sendData(sendObject);
+
+                        console.log("awwd", userReceiveAddress)
+
+                        // await WalletModel
+                        //     .query()
+                        //     .insert({
+                        //         "receive_address": userReceiveAddress,
+                        //         "send_address": userSendAddress,
+                        //         "coin_id": coinData.id,
+                        //         "user_id": user_id,
+                        //         "deleted_at": null,
+                        //         "created_at": Date.now(),
+                        //         "wallet_id": "wallet",
+                        //         "address_label": label,
+                        //         "balance": 0.0,
+                        //         "placed_balance": 0.0
+                        //     })
+                        return res
+                            .status(200)
+                            .json({
+                                "status": 200,
+                                "message": "Send Coins successfully."
+                            })
+                    } else {
+                        return res
+                            .status(400)
+                            .json({
+                                "status": 400,
+                                "message": "Wallet Data Not Found"
+                            })
                     }
-
-                    var userReceiveAddress = await sendHelper.sendData(sendObject);
-
-                    console.log("awwd", userReceiveAddress)
-
-                    // await WalletModel
-                    //     .query()
-                    //     .insert({
-                    //         "receive_address": userReceiveAddress,
-                    //         "send_address": userSendAddress,
-                    //         "coin_id": coinData.id,
-                    //         "user_id": user_id,
-                    //         "deleted_at": null,
-                    //         "created_at": Date.now(),
-                    //         "wallet_id": "wallet",
-                    //         "address_label": label,
-                    //         "balance": 0.0,
-                    //         "placed_balance": 0.0
-                    //     })
-                    return res
-                        .status(200)
-                        .json({
-                            "status": 200,
-                            "message": "Send Coins successfully."
-                        })
-                } else {
-                    return res
-                        .status(400)
-                        .json({
-                            "status": 400,
-                            "message": "Wallet has already been created"
-                        })
-                }
+                
             } else {
                 return res
-                    .status(500)
-                    .json({
-                        "status": 500,
-                        "message": "Coin Not Found"
-                    })
-            }
+                        .status(500)
+                        .json({
+                            "status": 500,
+                            "message": "Coin Not Found"
+                        })
+            }    
         } catch (error) {
             console.log(error)
         }
