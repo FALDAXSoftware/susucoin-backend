f99e8e60dc70665e9c4f02738cdbdbfc407848ee
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-02-20 14:21:30 +0530

    susucoin receive and wallet new flow implementation

--- controllers/v1/CoinsController.js
+++ controllers/v1/CoinsController.js
@@ -469,7 +469,7 @@ class UsersController extends AppController {
                         var valiueIm = (dataTransactionValue['vout']);
                         sourcxeAddressValue = valiueIm[0]['scriptPubKey']['addresses'][0]
                         if (sourcxeAddressValue == dataValue[i].address) {
-                            sourcxeAddressValue = valiueIm[0]['scriptPubKey']['addresses'][1]
+                            sourcxeAddressValue = valiueIm[1]['scriptPubKey']['addresses'][1]
                         }
                         var walletHistoryData = await WalletHistoryModel
                             .query()
